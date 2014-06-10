96e1c0da0b2daccd8ae71db25320bc43d94bdece
グミ <hi@gumi.nu>
2014-06-10 10:39:13 -0700

    omg it works

--- src/main/java/nu/gumi/perfume/perfume.java
+++ src/main/java/nu/gumi/perfume/perfume.java
@@ -4,4 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class perfume extends JavaPlugin{
 
+	@Override
+	public void onEnable() {
+		getLogger().info("onEnable has been invoked!");
+
+	}
+	 
+	@Override
+	public void onDisable() {
+		getLogger().info("onDisable has been invoked!");
+	}
+
 }
