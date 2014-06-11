b2750a3b4056a269b80f81cda332b34fb0e74d4f
グミ <hi@gumi.nu>
2014-06-10 21:37:08 -0700

    command works in game

--- src/main/java/nu/gumi/perfume/Perfume.java
+++ src/main/java/nu/gumi/perfume/Perfume.java
@@ -1,5 +1,8 @@
 package nu.gumi.perfume;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Perfume extends JavaPlugin{
@@ -7,7 +10,6 @@ public final class Perfume extends JavaPlugin{
 	@Override
 	public void onEnable() {
 		getLogger().info("onEnable has been invoked!");
-
 	}
 	 
 	@Override
@@ -15,4 +17,23 @@ public final class Perfume extends JavaPlugin{
 		getLogger().info("onDisable has been invoked!");
 	}
 
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("perfume")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("This command can only be run by a player.");
+			} else {
+				if (args.length != 0) {
+			           sender.sendMessage("No arguments allowed!");
+			           return false;
+				} else {
+					Player player = (Player) sender;
+					player.sendMessage("harro");
+				}
+			}
+		}
+		
+		return false; 
+	}
+	
 }
